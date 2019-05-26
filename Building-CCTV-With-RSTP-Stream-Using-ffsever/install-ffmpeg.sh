20b3fcae4c500c1d0b5b04f8a3634cffef85b530
HollowMan6 <jiangsl18@lzu.edu.cn>
2019-05-26 16:01:06 +0800

    Latest push

--- Building-CCTV-With-RSTP-Stream-Using-ffsever/install-ffmpeg.sh
+++ Building-CCTV-With-RSTP-Stream-Using-ffsever/install-ffmpeg.sh
@@ -1,3 +1,4 @@
+#!/bin/bash 
 echo "安装ffmpeg3.4.6, 请确保所有deb文件位于当前shell打开的目录下的ffmpeg3.4.6文件夹下"
 echo "Install ffmpeg 3.4.6. Make sure that all DEB files are in the ffmpeg 3.4.6 folder under the directory currently opened by the shell"
 dpkg -i ./ffmpeg3.4.6/*.deb
